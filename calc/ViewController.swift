dbca0c74d5c1b0cfe3eedb51525604d08819bb36
Vladislav Bliznyuk <braacy@yandex.ru>
2025-09-02 14:08:06 +0200

    Initial commit

---
+++ calc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  calc
+//
+//  Created by Владислав Близнюк on 19.08.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
