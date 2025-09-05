292f3549ae68aec885154017984512d8cc937884
Vladislav Bliznyuk <braacy@yandex.ru>
2025-09-05 16:17:50 +0200

    added first catch errors

--- calc/ViewController.swift
+++ calc/ViewController.swift
@@ -7,20 +7,135 @@
 
 import UIKit
 
+enum CalculatorError: Error {
+    case dividedByZero
+}
+
+enum Operation: String {
+    case add = "+"
+    case subtract = "-"
+    case multiply = "X"
+    case divide = "/"
+    
+    func calculate(_ a: Double, _ b: Double) throws -> Double {
+        switch self {
+        case .add:
+            return a + b
+        case .subtract:
+            return a - b
+        case .multiply:
+            return a * b
+        case .divide:
+            if b == 0 {
+                throw CalculatorError.dividedByZero
+            }
+            return a / b
+        }
+    }
+}
+
+enum CalculatorHistory {
+    case number(Double)
+    case operation(Operation)
+}
+
 class ViewController: UIViewController {
     
     @IBAction func buttonPressed(_ sender: UIButton) {
         guard let buttonText = sender.titleLabel?.text else { return }
         
-        print(buttonText)
+        if buttonText == "," && label.text?.contains(",") == true {
+            return
+        }
+        
+        if label.text == "0" {
+            label.text = buttonText
+        } else {
+            label.text?.append(buttonText)
+        }
     }
+    
+    @IBAction func operationbuttonPressed(_ sender: UIButton) {
+        guard
+        let buttonText = sender.titleLabel?.text,
+        let buttonOperation = Operation(rawValue: buttonText)
+        else { return }
+        
+        guard
+        let labelText = label.text,
+        let labelNumber = numberFormatter.number(from: labelText)?.doubleValue
+        else {return}
+        
+        calculatorHistory.append(.number(labelNumber))
+        calculatorHistory.append(.operation(buttonOperation))
+        
+        resetTextLabel()
+        }
+    
+    @IBAction func clearbuttonPressed() {
+        calculatorHistory.removeAll()
+        
+        resetTextLabel()
+    }
+    
+    @IBAction func calculatebuttonPressed() {
+        guard
+        let labelText = label.text,
+        let labelNumber = numberFormatter.number(from: labelText)?.doubleValue
+        else {return}
+        
+        calculatorHistory.append(.number(labelNumber))
+        do {
+            let result = try calculate()
+            label.text = numberFormatter.string(from: NSNumber(value: result))
+        } catch {
+            label.text = "Error"
+        }
+        calculatorHistory.removeAll()
+        
+    }
+    
+    @IBOutlet weak var label: UILabel!
+    
+    var calculatorHistory: [CalculatorHistory] = []
+    
+    lazy var numberFormatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.usesGroupingSeparator = false
+        formatter.locale = Locale(identifier: "ru_RU")
+        formatter.numberStyle = .decimal
+        
+        return formatter
+    }()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print("та дам")
+     resetTextLabel()
     }
-
+    
+    func calculate() throws -> Double {
+        guard case .number(let firstNumber) = calculatorHistory[0] else { return 0 }
+        
+        var currentResult = firstNumber
+        
+        for index in stride(from: 1, to: calculatorHistory.count - 1, by: 2) {
+            guard
+            case .operation(let operation) = calculatorHistory[index],
+            case .number(let number) = calculatorHistory[index + 1]
+            else { break }
+            
+            currentResult = try operation.calculate(currentResult, number)
+        }
+        
+        
+        return currentResult
+    }
+    
+    func resetTextLabel() {
+        label.text = "0"
+    }
 
 }
 
